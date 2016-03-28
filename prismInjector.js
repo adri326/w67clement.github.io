15fbaad731bdd02e3502b78f1ba8b80c3e4e302d
w67clement <w67clement@gmail.com>
2016-03-28 17:22:52 +0200

    Fix bug in prismInjector.js

--- prismInjector.js
+++ prismInjector.js
@@ -1,10 +1,3 @@
 // Prism.js injector for forums.
-var prismCss = document.createElement("link");
-prismCss.setAttribute('rel', 'stylesheet');
-prismCss.setAttribute('href', 'https://w67clement.github.io/prism.css');
-
-var prismJs = document.createElement("script");
-prismJs.setAttribute('src', 'https://w67clement.github.io/prism.js');
-
-$('head').append(prismCss);
-$('body').append(prismJs);
+$('head').append("<link rel=\"stylesheet\" href=\"https://w67clement.github.io/prism.css\" />");
+$('body').append("<script type=\"text/javascript\" src=\"https://w67clement.github.io/prism.js\"");
