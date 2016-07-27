fb4ea8ac3dfe89af1733b585083781283f090fd4
w67clement <w67clement@gmail.com>
2016-07-27 15:03:14 +0200

    Add some stuff

--- test/js/lang.js
+++ test/js/lang.js
@@ -1,2 +1,9 @@
-var fr_FRContent;
-var en_USContent;
+function gup(name, url)
+{
+    if (!url) url = location.href;
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( url );
+    return results == null ? null : results[1];
+}
